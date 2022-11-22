49f45de031c840400943db247a985d25d46def80
user <smartleejunghyun@gmail.com>
2022-11-22 15:16:45 +0900

    speak 삭제

--- src/main/resources/static/js/listen.js
+++ src/main/resources/static/js/listen.js
@@ -8,28 +8,6 @@
  var audio = new Audio('js/testvoice.mp3');
 
  
- const speechMsg = new SpeechSynthesisUtterance()
- console.dir(window.speechSynthesis.getVoices());
- function speak(text) {
-	
-
-	window.speechSynthesis.cancel() // 현재 읽고있다면 초기화
-
-	
-
-	
-	speechMsg.rate =  1 // 속도: 0.1 ~ 10      
-	speechMsg.pitch = 1 // 음높이: 0 ~ 2
-	speechMsg.lang =  "ko-KR"
-	speechMsg.text = text
-
-	// SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
-	window.speechSynthesis.speak(speechMsg)
-}
-
-
-
-
 
 
 // index 화면
