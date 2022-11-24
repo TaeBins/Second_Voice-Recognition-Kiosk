66bedcbb009ce54a84b35b43938cda952deb79dc
bin <bin@example.com>
2022-11-24 17:30:31 +0900

    Merge branch 'master' of https://github.com/2022-SMHRD-KDT-DCX-BigData-3/KeysCore.git

--- src/main/java/com/KSCT/work/controller/IndexController.java
+++ src/main/java/com/KSCT/work/controller/IndexController.java
@@ -80,10 +80,7 @@ public class IndexController {
 //			return sl;
 //			
 //		}
-	}
 	
-
-<<<<<<< HEAD
 	@GetMapping("/animation")
 	public String ani() {
 		return "animation";
@@ -95,7 +92,5 @@ public class IndexController {
 	}
 
 }
-=======
->>>>>>> branch 'master' of https://github.com/2022-SMHRD-KDT-DCX-BigData-3/KeysCore.git
 
 
