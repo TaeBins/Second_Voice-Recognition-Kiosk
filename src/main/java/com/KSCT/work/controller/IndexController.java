5e2268479146715f818f63b4b6a2f0234c5eb7d0
DESKTOP-1JL9J5N\junghyun <orcteacher@naver.com>
2022-11-29 11:36:20 +0900

    1

--- src/main/java/com/KSCT/work/controller/IndexController.java
+++ src/main/java/com/KSCT/work/controller/IndexController.java
@@ -12,11 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.ResponseBody;
-=======
+
 import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> branch 'master' of https://github.com/2022-SMHRD-KDT-DCX-BigData-3/KeysCore.git
 
 import com.KSCT.work.model.Menus;
 import com.KSCT.work.model.Orders;
