9a5f7de56927b71ac55cd5e999c9614160e33425
user <smartleejunghyun@gmail.com>
2022-12-02 14:36:15 +0900

    Merge branch 'master' of https://github.com/2022-SMHRD-KDT-DCX-BigData-3/KeysCore.git

---
+++ src/main/java/com/KSCT/work/model/TableInfo.java
@@ -0,0 +1,13 @@
+package com.KSCT.work.model;
+
+import lombok.Data;
+
+@Data
+public class TableInfo {
+
+	private int tbl_number;
+	
+	private int receipt_number;
+	
+	
+}
--- src/main/java/com/KSCT/work/service/IndexService.java
+++ src/main/java/com/KSCT/work/service/IndexService.java
@@ -2,6 +2,8 @@ package com.KSCT.work.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,7 @@ import com.KSCT.work.mapper.IndexMapper;
 import com.KSCT.work.model.Menus;
 import com.KSCT.work.model.Orders;
 import com.KSCT.work.model.Receipt;
+import com.KSCT.work.model.TableInfo;
 
 // @Transactional : 메소드 내부에서 일어나는 DB 로직이 전부성공 or 하나라도 실패시 다시롤백 해줌
 // @Service : 서비스 Class에 사용
@@ -19,6 +22,15 @@ public class IndexService {
 
 	@Autowired
 	private IndexMapper indexMapper;
+	
+	
+	//화면이 켜지면 테이블 값 가져오고, receipt_num이 없다면 max+1 값으로 셋팅
+	public TableInfo getTable(HttpSession session) {
+		TableInfo table_info = indexMapper.getTable();
+		session.setAttribute("table", table_info);
+		return table_info;
+	}
+	
 	
 	//모든 메뉴 가져오기(컨트롤에서 보낸 menu_type int 형으로)
 	public List<Menus> menulist(Menus menus){
@@ -33,15 +45,26 @@ public class IndexService {
 
 	}
 
+
+	
 	//손님이 주문한 목록 DB에 저장하기 (오른쪽에 뜨는 메뉴목록)
-	public void order(Orders orders) {
+	public boolean order(Orders orders) {
 		
 		// 이번 순번에서 주문했었던 메뉴라면 update 시키기
+		//이 테이블이 기존에 주문했던 주문목록이 있는지 확인
 		
-		
+		int checkNum = indexMapper.checkReceiptNum(orders);
+		boolean addReceipt = false;
+		if(checkNum == 0) {
+			System.out.println("현재 하는 주문은 최초의 주문입니다. 영수증을 만듭니다.");
+			addReceipt = true;
+		} else {
+		System.out.println("현재 테이블은 영수증이 있습니다.");
+		}
 		// 이번 순번에서 처음으로 주문한 메뉴라면 insert 시키기
 		indexMapper.order(orders); 
 		
+		return addReceipt;
 		
 		
 		
@@ -69,6 +92,7 @@ public class IndexService {
 	
 	// 영수증 목록 가져오기
 	public List<Receipt> receiptList(){
+		
 		return indexMapper.receiptList();
 	}
 
